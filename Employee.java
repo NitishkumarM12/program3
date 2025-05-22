8cb9ff2f71a6c7dd357affd8949ec5222d86e899
NitishkumarM12 <mudaraddinitishkumar@outlook.com>
2025-05-22 18:53:30 +0530

    save file

--- Employee.java
+++ Employee.java
@@ -3,25 +3,19 @@ public class Employee {
     private String name;
     private int id;
     private double salary;
-    private String department;
-    private double salary;
 
-    public Employee(String name, int id, double salary, String department, double salary) {
+    public Employee(String name, int id, double salary) {
         this.name = name;
         this.id = id;
-        this.salary = salary;
-        this.department = department;
         this.salary = salary;
     }
     public void dispay() {
         System.out.println("Employee Name: " + name);
         System.out.println("Employee ID: " + id);
         System.out.println("Salary: " + salary);
-        System.out.println("Department: " + department);
-        System.out.println("Salary: " + salary);
     }
     public static void main(String[] args) {
-        Employee emp = new Employee("John Doe", 12345, 50000.0, "Engineering", 50000.0);
+        Employee emp = new Employee("John Doe", 12345, 50000.0);
         emp.dispay();
     }
 }
